b1e7e9ebe56aea76b85ea599e5501968e74d6910
Murilo <muriloflimaa@gmail.com>
2022-10-07 12:38:01 -0300

    first commit

---
+++ src/screens/Dashboard/index.tsx
@@ -0,0 +1,10 @@
+import { StyleSheet, Text, View } from "react-native";
+import { Container, Title } from "./styles";
+
+export function Dashboard() {
+  return (
+    <Container>
+      <Title>Dashboard</Title>
+    </Container>
+  );
+}
